d51629109dd3e2a8ec76627ddbdd41faf4867716
Denis Rajlic <denis.rajlic@gmail.com>
2021-05-15 23:16:26 +0200

    add auth middlewares

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,19 +1,12 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
+
+import { requireAuth } from '../middleware/require-auth';
+import { currentUser } from '../middleware/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
