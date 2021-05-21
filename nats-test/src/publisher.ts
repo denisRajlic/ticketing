b6101103461dd6224c32875bd250679c4e1fbe45
Denis Rajlic <denis.rajlic@gmail.com>
2021-05-21 18:36:12 +0200

    refactor publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 
@@ -10,13 +11,10 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: '33',
     title: 'concert',
     price: 20,
-  });
-
-  stan.publish('ticket:created', data, () => {
-    console.log('Event published');
   });
 });
