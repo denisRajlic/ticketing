100ba584a07bcd9a1b60d6c16d8f3ca72c57a526
Denis Rajlic <denis.rajlic@gmail.com>
2021-05-21 20:12:46 +0200

    finish NATS singleton implementation

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -13,6 +13,13 @@ const start = async () => {
       randomBytes(4).toString('hex'),
       'http://nats-srv:4222'
     );
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@tickets-tutorial/common';
 import { Ticket } from '../models/ticket';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -27,12 +28,12 @@ router.post(
 
     await ticket.save();
 
-    // new TicketCreatedPublisher(client).publish({
-    //   id: ticket.id,
-    //   title: ticket.title,
-    //   price: ticket.price,
-    //   userId: ticket.userId,
-    // });
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.status(201).send(ticket);
   }
